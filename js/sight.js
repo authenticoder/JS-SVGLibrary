64a4adf61c5dd00de7fdca25a6e4f7c84f21973e
authenticoder <authenticoderio@gmail.com>
2021-12-15 23:29:34 +0300

    m1 c6

--- js/sight.js
+++ js/sight.js
@@ -7,6 +7,9 @@ class SVGElement {
     }
 
     attr(attrs) {
+        for (const [key, value] of Object.entries(attrs)) {
+            this.node.setAttributeNS(null, key, value)
+        }
         return this;
     }
 }
